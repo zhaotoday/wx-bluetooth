130b26f7c25f13fcbeb8fc398bb58cd0fe3540ae
zhaojintian <729234283@qq.com>
2021-07-12 11:34:04 +0800

    update

--- composables/use-bluetooth-data-parser.js
+++ composables/use-bluetooth-data-parser.js
@@ -13,8 +13,8 @@ export const useBluetoothDataParser = () => {
   };
 
   // 16 进制转 10 进制
-  const hexToDecimal = (hex, unsigned = true) => {
-    return parseInt((unsigned ? "0000" : "FFFF") + hex.split(" ").join(""), 16);
+  const hexToDecimal = (hex, signed = true) => {
+    return parseInt((signed ? "0000" : "FFFF") + hex.split(" ").join(""), 16);
   };
 
   // 16 进制转 2 进制
