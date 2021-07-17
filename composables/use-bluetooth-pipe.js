276244a55b1db251ebf21d75919ad8e1b1f8f457
zhaojintian <729234283@qq.com>
2021-07-17 16:49:10 +0800

    update

--- composables/use-bluetooth-pipe.js
+++ composables/use-bluetooth-pipe.js
@@ -12,6 +12,69 @@ export const useBluetoothPipe = () => {
       .join(" ");
   };
 
+  // 按 length 个字符一组分割字符串
+  const splitString = (string, length = 2) => {
+    const ret = [];
+
+    for (let i = 0; i < string.length / length; i++) {
+      ret.push(string.substr(i * length, length));
+    }
+
+    return ret.join(" ");
+  };
+
+  // 10 进制转 16 进制
+  // 参考：https://blog.csdn.net/qq_15801963/article/details/108110061
+  const decimalToHex = (decimal, length, unsigned = false) => {
+    var num = parseInt(decimal);
+    var hex = "";
+    var hexLength = length * 2; //1个字节=2个16进制字符
+    var min = 0;
+    var max = 0;
+    if (!unsigned) {
+      //带符号位
+      min = -1 * Math.pow(2, 8 * length - 1);
+      max = Math.pow(2, 8 * length - 1) - 1;
+      if (num < min || num > max) {
+        //数字超出转换范围
+        console.warn(
+          `out of range,min is ${min},max is ${max},but decimal is ${num}.`
+        );
+      } else if (num < 0) {
+        //传入合法负数
+        //计算补码
+        hex = (Math.pow(2, 8 * length) + num).toString(16);
+        if (hex.length < hexLength) {
+          hex = "f".repeat(hexLength - hex.length) + hex;
+        }
+      } else {
+        //传入合法正数
+        hex = num.toString(16);
+        if (hex.length < hexLength) {
+          hex = "0".repeat(hexLength - hex.length) + hex;
+        }
+      }
+    } else {
+      //不带符号位
+      min = 0;
+      max = Math.pow(2, 8 * length) - 1;
+      if (num < min || num > max) {
+        //数字超出转换范围
+        console.warn(
+          `out of range,min is ${min},max is ${max},but decimal is ${num}.`
+        );
+      } else {
+        //传入合法正数
+        hex = num.toString(16);
+        if (hex.length < hexLength) {
+          hex = "0".repeat(hexLength - hex.length) + hex;
+        }
+      }
+    }
+
+    return splitString(hex, 2);
+  };
+
   // 16 进制转有符号 10 进制
   // 参考：https://www.cnblogs.com/BlackFungus/p/9473008.html
   const hexToDecimal = (hex, unsigned = false) => {
@@ -54,18 +117,6 @@ export const useBluetoothPipe = () => {
   // 16 进制转 10 进制数组
   const hexToDecimalArray = (hex) => {
     return hex.split(" ").map((item) => hexToDecimal(item, true));
-  };
-
-  // 16 进制转 10 进制
-  const decimalToHex = (decimal, length) => {
-    const string = fillZero(parseInt(decimal).toString(16), length);
-    const ret = [];
-
-    for (let i = 0; i < string.length / 2; i++) {
-      ret.push(string.substr(i * 2, 2));
-    }
-
-    return ret.join(" ");
   };
 
   // 16 进制求和
